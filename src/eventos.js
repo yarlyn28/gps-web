ee7c292bbe0496bbae896d7b8b04eeae3b46ae59
Jose Guevara <94gjose@gmail.com>
2023-03-19 22:19:38 -0400

    fix: arreglar problemas con mapa renderizando incorrectamente

--- src/eventos.js
+++ src/eventos.js
@@ -2,35 +2,41 @@ import { OCULTAR } from "./navegacion";
 import { mostrarMapa } from "./mapa";
 
 let clean = null;
-
+let localizacionesActuales;
 export function ON_ActualizarLocalizaciones(localizaciones) {
   if (clean !== null) {
     clean();
   }
+  localizacionesActuales = localizaciones;
   clean = mostrarMapa(localizaciones);
 }
 
 const paginaLogin = document.getElementById("login-page");
 const paginaApp = document.getElementById("app-page");
 
-paginaLogin.classList.add(OCULTAR);
-paginaApp.classList.remove(OCULTAR);
+paginaApp.classList.add(OCULTAR);
+paginaLogin.classList.remove(OCULTAR);
 
-const usuarioTexto = document.getElementById("usuario-nombre")
-const usuarioImagen = document.getElementById("usuario-imagen")
+const usuarioTexto = document.getElementById("usuario-nombre");
+const usuarioImagen = document.getElementById("usuario-imagen");
 
-const usuarioTexto2 = document.getElementById("usuario-nombre-2")
-const usuarioImagen2 = document.getElementById("usuario-imagen-2")
+const usuarioTexto2 = document.getElementById("usuario-nombre-2");
+const usuarioImagen2 = document.getElementById("usuario-imagen-2");
 
-const urlNoProfile = "https://www.transparentpng.com/thumb/user/gray-user-profile-icon-png-fP8Q1P.png"
+const urlNoProfile =
+  "https://www.transparentpng.com/thumb/user/gray-user-profile-icon-png-fP8Q1P.png";
 export function ON_Login({ nombre, imagen }) {
   paginaLogin.classList.add(OCULTAR);
   paginaApp.classList.remove(OCULTAR);
 
-  usuarioTexto.innerHTML = nombre
-  usuarioTexto2.innerHTML = nombre
-  usuarioImagen.src = null ?? urlNoProfile
-  usuarioImagen2.src = null ?? urlNoProfile
+  usuarioTexto.innerHTML = nombre;
+  usuarioTexto2.innerHTML = nombre;
+  usuarioImagen.src = null ?? urlNoProfile;
+  usuarioImagen2.src = null ?? urlNoProfile;
+
+  if (localizacionesActuales !== null) {
+    ON_ActualizarLocalizaciones(localizacionesActuales)
+  }
 }
 
 export function ON_Logout() {
